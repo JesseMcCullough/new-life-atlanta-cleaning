4529fda7dd1a7ae07f3aae430f6cf18737a52d71
Jesse McCullough <jesse@jessem.dev>
2022-10-04 12:17:30 -0400

    Added navigation bar

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="styles/style.css" />
+    <title>New Life Atlanta Cleaning</title>
+</head>
+<body>
+    <div class="nav">
+        <a href="#" class="logo"><img src="images/logo.png" /></a>
+        <ul>
+            <li><a href="#">Tile</a></li>
+            <li><a href="#">Carpet</a></li>
+            <li><a href="#">Upholstery</a></li>
+            <li><a href="#">Rug</a></li>
+            <li><a href="#">Hardwood</a></li>
+        </ul>
+        <div class="phone-cta">
+            <a href="tel:(678) 318-1353" class="phone">
+                <img src="images/icons/phone-icon.png" />
+                (678) 318-1353
+            </a>
+            <a href="#" class="button">Schedule</a>
+        </div>
+    </div>
+    <section class="service-selector-slide">
+
+    </section>
+</body>
+</html>
