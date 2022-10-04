d6d320aed68783dc535b7d93e6f2e9108c9aae71
Jesse McCullough <jesse@jessem.dev>
2022-10-04 18:20:48 -0400

    Added About section

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <li><a href="#">Hardwood</a></li>
         </ul>
         <div class="phone-cta">
-            <a href="tel:(678) 318-1353" class="phone">
+            <a href="tel:(678)318-1353" class="phone">
                 <img src="images/icons/phone-icon.png" />
                 (678) 318-1353
             </a>
@@ -43,6 +43,22 @@
         </div>
         <a href="#" class="button secondary">Learn about carpet cleaning</a>
         <a href="#" class="button">Schedule carpet cleaning</a>
+    </section>
+    <section class="about">
+        <div class="container">
+            <div class="text">
+                <h2>About Us</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel fermentum felis. Pellentesque malesuada elit eget pulvinar mollis. Maecenas sit amet mauris et elit rhoncus commodo. Sed vitae ornare arcu, non rhoncus nulla. Vivamus euismod rutrum ante vitae cursus. Etiam lorem ipsum, convallis non lacus a, malesuada porta turpis. Suspendisse potenti. Vivamus ut mi lorem. Fusce a odio sapien. Donec nec volutpat sem, id suscipit nibh. Pellentesque dui quam, ullamcorper quis vestibulum ultricies, commodo ut mi. Aenean et scelerisque urna, vel placerat dui. Maecenas tincidunt lectus quis tempus pretium.</p>
+                <p>Quisque maximus maximus ex. Vivamus rutrum egestas metus, vel fringilla odio iaculis eget. In aliquet arcu arcu. Aenean lorem mi, pulvinar in justo nec, convallis ornare est. Aliquam et vestibulum mauris, ultricies pellentesque eros. Maecenas fermentum dui feugiat velit hendrerit lacinia. Morbi ultrices tellus sit amet mauris consectetur, ac condimentum tellus suscipit. Mauris nec neque finibus, interdum odio vitae, sodales urna.</p>
+            </div>
+            <div class="image">
+                <div class="caption">
+                    <span id="name">Ken &amp; Penny McCullough</span>
+                    <span id="title">Owners / Operators</span>
+                </div>
+                <img src="images/ken-penny.png" />
+            </div>
+        </div>
     </section>
 </body>
 </html>
