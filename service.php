d4f5ea61d461cb5f625e18e4964a36b76ea027f2
Jesse McCullough <jesse@jessem.dev>
2022-11-17 16:25:00 -0500

    Added service page

--- service.php
+++ service.php
@@ -1,6 +1,6 @@
 <?php include_once("includes/header.php"); ?>
 
-<section class="service-selector">
+<section class="service-selector before-after">
     <div class="overlay"></div>
     <div class="background-img-1"></div>
     <div class="background-img-2"></div>
@@ -10,40 +10,42 @@
         <div class="service-selector-slide">
             <div class="left-arrow"><span>&lt;</span></div>
             <div class="items">
-                <div class="item-container left tile" data-id="0" data-title="Tile"
-                        data-button-text="tile cleaning" data-button-href="#tilecleaning"
+                <div class="item-container middle" data-id="0"
+                        data-background-image="images/service-selector/carpet-1.png"
+                        data-overlay-opacity="0.5">
+                    <div class="item-content before" style="background-image: url('images/service-selector/carpet-1-before-demo.png')">1</div>
+                    <div class="item-content after" style="background-image: url('images/service-selector/carpet-1.png')"></div>
+                </div>
+                <div class="item-container invisible" data-id="1"
                         data-background-image="images/service-selector/vct-1.png"
                         data-overlay-opacity="0.5">
-                    <div class="item-content"></div>
+                    <div class="item-content before" style="background-image: url('images/service-selector/carpet-1-before-demo.png')">2</div>
+                    <div class="item-content after" style="background-image: url('images/service-selector/carpet-1.png')"></div>
                 </div>
-                <div class="item-container middle carpet" data-id="1" data-title="Carpet"
-                        data-button-text="carpet cleaning" data-button-href="#carpetcleaning"
-                        data-background-image="images/service-selector/carpet-1.png">
-                    <div class="item-content"></div>
+                <div class="item-container invisible" data-id="2"
+                        data-background-image="images/service-selector/carpet-1.png"
+                        data-overlay-opacity="0.5">
+                    <div class="item-content before" style="background-image: url('images/service-selector/carpet-1-before-demo.png')">3</div>
+                    <div class="item-content after" style="background-image: url('images/service-selector/carpet-1.png')"></div>
                 </div>
-                <div class="item-container right upholstery" data-id="2" data-title="Upholstery"
-                        data-button-text="upholstery cleaning" data-button-href="#upholsterycleaning"
-                        data-background-image="images/service-selector/upholstery-1.png"
-                        data-overlay-opacity="0.4">
-                    <div class="item-content"></div>
+                <div class="item-container invisible" data-id="3"
+                        data-background-image="images/service-selector/vct-1.png"
+                        data-overlay-opacity="0.5">
+                    <div class="item-content before" style="background-image: url('images/service-selector/carpet-1-before-demo.png')">4</div>
+                    <div class="item-content after" style="background-image: url('images/service-selector/carpet-1.png')"></div>
                 </div>
-                <div class="item-container invisible service-4" data-id="3" data-title="Service 4"
-                        data-button-text="service 4 cleaning" data-button-href="#service4cleaning"
-                        data-background-image="images/service-selector/vct-2.png"
-                        data-overlay-opacity="0.8">
-                    <div class="item-content"></div>
-                </div>
-                <div class="item-container invisible service-5" data-id="4" data-title="Service 5"
-                        data-button-text="service 5 cleaning" data-button-href="#service5cleaning"
-                        data-background-image="images/service-selector/carpet-2.png">
-                    <div class="item-content"></div>
+                <div class="item-container invisible" data-id="4"
+                        data-background-image="images/service-selector/vct-1.png"
+                        data-overlay-opacity="0.5">
+                    <div class="item-content before" style="background-image: url('images/service-selector/carpet-1-before-demo.png')">5</div>
+                    <div class="item-content after" style="background-image: url('images/service-selector/carpet-1.png')"></div>
                 </div>
             </div>
             <div class="right-arrow"><span>&gt;</span></div>
         </div>
         <div class="slide-dots">
-            <div class="dot" data-id="0"></div>
-            <div class="dot active" data-id="1"></div>
+            <div class="dot active" data-id="0"></div>
+            <div class="dot" data-id="1"></div>
             <div class="dot" data-id="2"></div>
             <div class="dot" data-id="3"></div>
             <div class="dot" data-id="4"></div>
@@ -60,35 +62,39 @@
         <p class="stat"><span class="highlight">40+</span>years of <br />experience</p>
     </div>
 </section>
-<section class="about">
+<section class="about service">
     <div class="container">
         <div class="text">
-            <h2>About Us</h2>
+            <h2>Lorem ipsum</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel fermentum felis. Pellentesque malesuada elit eget pulvinar mollis. Maecenas sit amet mauris et elit rhoncus commodo. Sed vitae ornare arcu, non rhoncus nulla. Vivamus euismod rutrum ante vitae cursus. Etiam lorem ipsum, convallis non lacus a, malesuada porta turpis. Suspendisse potenti. Vivamus ut mi lorem. Fusce a odio sapien. Donec nec volutpat sem, id suscipit nibh. Pellentesque dui quam, ullamcorper quis vestibulum ultricies, commodo ut mi. Aenean et scelerisque urna, vel placerat dui. Maecenas tincidunt lectus quis tempus pretium.</p>
             <p>Quisque maximus maximus ex. Vivamus rutrum egestas metus, vel fringilla odio iaculis eget. In aliquet arcu arcu. Aenean lorem mi, pulvinar in justo nec, convallis ornare est. Aliquam et vestibulum mauris, ultricies pellentesque eros. Maecenas fermentum dui feugiat velit hendrerit lacinia. Morbi ultrices tellus sit amet mauris consectetur, ac condimentum tellus suscipit. Mauris nec neque finibus, interdum odio vitae, sodales urna.</p>
         </div>
         <div class="image">
-            <div class="caption">
-                <span class="name">Ken &amp; Penny McCullough</span>
-                <span class="title">Owners / Operators</span>
-            </div>
-            <img src="images/ken-penny.png" />
+            <img src="images/service-selector/carpet-1.png" />
         </div>
     </div>
 </section>
-<section class="about">
+<section class="about service">
     <div class="container">
         <div class="text">
-            <h2>About Us</h2>
+            <h2>Lorem ipsum</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel fermentum felis. Pellentesque malesuada elit eget pulvinar mollis. Maecenas sit amet mauris et elit rhoncus commodo. Sed vitae ornare arcu, non rhoncus nulla. Vivamus euismod rutrum ante vitae cursus. Etiam lorem ipsum, convallis non lacus a, malesuada porta turpis. Suspendisse potenti. Vivamus ut mi lorem. Fusce a odio sapien. Donec nec volutpat sem, id suscipit nibh. Pellentesque dui quam, ullamcorper quis vestibulum ultricies, commodo ut mi. Aenean et scelerisque urna, vel placerat dui. Maecenas tincidunt lectus quis tempus pretium.</p>
             <p>Quisque maximus maximus ex. Vivamus rutrum egestas metus, vel fringilla odio iaculis eget. In aliquet arcu arcu. Aenean lorem mi, pulvinar in justo nec, convallis ornare est. Aliquam et vestibulum mauris, ultricies pellentesque eros. Maecenas fermentum dui feugiat velit hendrerit lacinia. Morbi ultrices tellus sit amet mauris consectetur, ac condimentum tellus suscipit. Mauris nec neque finibus, interdum odio vitae, sodales urna.</p>
         </div>
         <div class="image">
-            <div class="caption">
-                <span class="name">Ken &amp; Penny McCullough</span>
-                <span class="title">Owners / Operators</span>
-            </div>
-            <img src="images/ken-penny.png" />
+            <img src="images/service-selector/carpet-1.png" />
+        </div>
+    </div>
+</section>
+<section class="about service">
+    <div class="container">
+        <div class="text">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel fermentum felis. Pellentesque malesuada elit eget pulvinar mollis. Maecenas sit amet mauris et elit rhoncus commodo. Sed vitae ornare arcu, non rhoncus nulla. Vivamus euismod rutrum ante vitae cursus. Etiam lorem ipsum, convallis non lacus a, malesuada porta turpis. Suspendisse potenti. Vivamus ut mi lorem. Fusce a odio sapien. Donec nec volutpat sem, id suscipit nibh. Pellentesque dui quam, ullamcorper quis vestibulum ultricies, commodo ut mi. Aenean et scelerisque urna, vel placerat dui. Maecenas tincidunt lectus quis tempus pretium.</p>
+            <p>Quisque maximus maximus ex. Vivamus rutrum egestas metus, vel fringilla odio iaculis eget. In aliquet arcu arcu. Aenean lorem mi, pulvinar in justo nec, convallis ornare est. Aliquam et vestibulum mauris, ultricies pellentesque eros. Maecenas fermentum dui feugiat velit hendrerit lacinia. Morbi ultrices tellus sit amet mauris consectetur, ac condimentum tellus suscipit. Mauris nec neque finibus, interdum odio vitae, sodales urna.</p>
+        </div>
+        <div class="image">
+            <img src="images/service-selector/carpet-1.png" />
         </div>
     </div>
 </section>
@@ -110,3 +116,5 @@ include_once("includes/cta-full-screen.php");
 include_once("includes/footer.php");
 
 ?>
+
+<script src="scripts/before-after.js"></script>
